8f7b5947fc5b7373394d94af72fee338d910c2ec
Maxuel Assis <maxuelassis95@gmail.com>
2024-09-27 21:05:08 -0300

    feat(migrations): create initial migrations for contracts, payments, and users tables

---
+++ api/database/migrations/2024_09_27_233124_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('contract_id');
+            $table->foreign('contract_id')->on('contracts')->references('id')->onDelete('cascade');
+
+            $table->decimal('amount', 10,2);
+            $table->date('paymwnt_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
